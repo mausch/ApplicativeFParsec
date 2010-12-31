bfc17feb686435cfd72467a23e5cb5f06a82647a
mausch <mauricioscheffer@gmail.com>
2010-12-31 15:43:48 -0300

    applicative fparsec

---
+++ ApplicativeFParsec/Applicative.fs
@@ -0,0 +1,43 @@
+﻿module FParsec.Applicative
+
+open FParsec.Primitives
+
+/// Sequential application
+let inline (<*>) f a = 
+    f >>= fun f' -> a >>= fun a' -> preturn (f' a')
+    (*
+    parse { 
+        let! f' = f 
+        let! a' = a 
+        return f' a' }
+    *)
+
+/// A variant of (<*>) with the arguments reversed
+let inline (<**>) a f = f <*> a
+
+/// Lifts a value. Equivalent to Haskell's 'pure' for parsers
+let inline puree a = preturn a
+
+/// Lift a function. Equivalent to Haskell's 'liftA' or 'fmap' for parsers
+let inline lift f a = a |>> f
+
+/// Lift a function. 
+let inline liftA x = lift x
+
+/// Lift a function. Equivalent to Haskell's (<$>) for parsers
+let inline (<!>) f a = lift f a // puree f <*> a
+
+/// Lift a binary function. Equivalent to Haskell's 'liftA2' for parsers
+let inline lift2 f a b = pipe2 a b f  //preturn f <*> a <*> b
+
+/// Lift a binary function. 
+let inline liftA2 x = lift2 x
+
+/// Replace all locations in the input with the same value. Equivalent to Haskell's (<$) for parsers
+let inline (<!) x y = y >>% x // (lift << (fun a b -> a)) x y
+
+/// Sequence parsers, discarding the value of the first parser.
+let inline ( *>) x y = x >>. y // lift2 (fun _ z -> z) x y
+
+/// Sequence parsers, discarding the value of the second parser.
+let inline ( <*) x y = x .>> y // lift2 (fun z _ -> z) x y
---
+++ ApplicativeFParsec/Program.fs
@@ -0,0 +1,37 @@
+﻿open System
+open FParsec.CharParsers
+open FParsec.Primitives
+open FParsec.Applicative
+
+let readHex a b = 
+    char <| Byte.Parse(sprintf "%c%c" a b, System.Globalization.NumberStyles.HexNumber)
+
+let p_hex : Parser<char, unit> = 
+    parse { 
+        do! skipChar '%' 
+        let! a = hex 
+        let! b = hex 
+        return readHex a b 
+    }
+
+let a_hex : Parser<char, unit> = 
+    puree (fun skipCharResult hex1result hex2result -> readHex hex1result hex2result) <*> skipChar '%' <*> hex <*> hex
+
+let a_hex2 : Parser<char, unit> = 
+    (fun skipCharResult hex1result hex2result -> readHex hex1result hex2result) <!> skipChar '%' <*> hex <*> hex
+
+let a_hex3 : Parser<char, unit> = 
+    (fun hex1result hex2result -> readHex hex1result hex2result) <!> skipChar '%' *> hex <*> hex
+
+let a_hex4 : Parser<char, unit> = 
+    readHex <!> skipChar '%' *> hex <*> hex 
+
+let a_hex5 : Parser<char, unit> = 
+    readHex |> pipe2 (skipChar '%' >>. hex) hex 
+
+match run p_hex "%20" with 
+| Success (result,_,_) -> 
+    if result = ' ' 
+        then printfn "Correctly parsed space" 
+        else failwith "Incorrecty parsed %c" result 
+| Failure (msg,_,_) -> failwithf "Parsing failed: \n%s" msg
